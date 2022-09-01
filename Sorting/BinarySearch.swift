52de4d9190191582ce046bf2856408a0653ff335
Ganesh Patil <ganeshbuddy.patil@gmail.com>
2022-09-01 17:15:35 +0530

    Create BinarySearch.swift

---
+++ Sorting/BinarySearch.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        var index = -1
+        while(left <= right) {
+            let mid: Int = (left + right) / 2
+            if (nums[mid] == target) {
+                index = mid
+                break
+            } else if (nums[mid] > target) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        return index
+    }
+}
